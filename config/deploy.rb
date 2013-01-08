aac3e1d3e05abc2e93a6615443f870297d1790e3
binaryseed <vincefoley@gmail.com>
2013-01-08 12:57:56 -0800

    deploy config update

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,10 +2,10 @@ load 'deploy' if respond_to?(:namespace) # cap2 differentiator
 
 
 
-$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
-require "rvm/capistrano"
-set :rvm_type, :user
-set :rvm_ruby_string, '1.9.2@comopolo'
+# $:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+# require "rvm/capistrano"
+# set :rvm_type, :user
+# set :rvm_ruby_string, '1.9.2@comopolo'
 
 set :user, "binaryseed"
 set :application, "comopolo"
@@ -20,38 +20,38 @@ set :deploy_via, :remote_cache
 
 
 set :server_ip, "node"
-set :deploy_to, "/home/binaryseed/srv/comopolo.com"
+set :deploy_to, "/home/binaryseed/apps/comopolo.com"
 server server_ip, :app, :web, :db, :primary => true
 
 
 namespace :deploy do
-	
+
 	desc "Restarting app"
 	task :restart do
 		run "touch #{current_path}/tmp/restart.txt"
 	end
-	
+
 	task :finalize_update do
 
 		run "[ -d #{release_path}/log ] || mkdir #{release_path}/log"
 		run "[ -d #{release_path}/tmp ] || mkdir #{release_path}/tmp"
-		
+
 	end
-	
+
 	task :symlink do
-		
+
 		run "rm -f #{current_path}"
 		run "ln -s #{release_path} #{current_path}"
-		
+
 		run "ln -s #{shared_path}/config.yml #{release_path}/config/config.yml"
 		run "ln -s #{shared_path}/database.yml #{release_path}/config/database.yml"
-		
+
 		run "ln -s #{shared_path}/avatars #{release_path}/public/avatars"
 		run "ln -s #{shared_path}/files #{release_path}/public/files"
 		run "ln -s #{shared_path}/headers #{release_path}/public/headers"
 		run "ln -s #{shared_path}/themes #{release_path}/public/themes"
 	end
-	
+
 end
 
 after "deploy", "deploy:cleanup"
